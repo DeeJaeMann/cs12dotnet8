3da96a2f44a4c7c012347910bb8df8696f3fb9e1
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-07 12:27:35 -0400

    Added comparisons for double and decimal Changes to be committed: 	modified:   Program.cs

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -26,3 +26,33 @@ Console.WriteLine($"{hexadecimalNotation:X}\n");
 Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.\n");
 Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range of {double.MinValue:N0} to {double.MaxValue:N0}.\n");
 Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range of {decimal.MinValue:N0} to {decimal.MaxValue:N0}.\n");
+
+// Comparisons
+Console.WriteLine("Using doubles:");
+// Only use double when accuracy is not important
+// Good for GT or LT comparison but not equals
+double a = 0.1;
+double b = 0.2;
+// Can not be compared
+if (a + b == 0.3)
+{
+    Console.WriteLine($"{a} + {b} equals {0.3}");
+}
+else
+{
+    Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
+}
+
+Console.WriteLine("Using decimals:");
+// Use decimal when accuracy of real numbers are important
+decimal c = 0.1M;
+decimal d = 0.2M;
+
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} equals {0.3M}.");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} does NOT equal {0.3M}.");
+}
