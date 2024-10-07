87924c3053faead592bd329c0e755682acc0cb28
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-07 13:24:19 -0400

    Added object and default output Changes to be committed: 	modified:   Program.cs 	new file:   something1.txt 	new file:   something2.txt

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,4 +1,6 @@
-﻿// Avoid these methods
+﻿using System.Xml;
+
+// Avoid these methods
 // Object types
 object height = 1.88; // store double in an object
 object name = "Amir"; // store string in an object
@@ -26,4 +28,49 @@ something = "Ahmed";
 
 // Compiles but may throw exception at run time
 Console.WriteLine($"The length of something is {something.Length}");
-Console.WriteLine($"something is a {something.GetType()}");
+Console.WriteLine($"something is a {something.GetType()}\n");
+
+// Local variables
+int population = 67_000_000;
+double weight = 1.88;
+decimal price = 4.99M;
+string fruit = "Apples";
+char letter = 'Z';
+bool happy = true;
+
+// can use var to declare to infer variables with previous declarations
+
+// Good use of var as to not repeat types
+var xml1 = new XmlDocument();
+
+XmlDocument xml2 = new XmlDocument();
+
+// Bad use of var can not tell type
+var file1 = File.CreateText("something1.txt");
+// Better readability for previous declaration
+StreamWriter file2 = File.CreateText("something2.txt");
+
+// C# 9 and later
+XmlDocument xml3 = new();
+
+Person kim = new();
+kim.BirthDate = new(1967, 12, 26); // new DateTime(1967, 12, 26)
+kim.FirstName = "Kim";
+
+List<Person> people = new()
+{
+    new() { FirstName = "Alice"},
+    new() { FirstName = "Bob"},
+    new() { FirstName = "Charlie"}
+};
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+class Person
+{
+    public DateTime BirthDate;
+    public string FirstName;
+}
