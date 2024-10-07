5faf96b8189072aa971e5cd9da9be73bbe021727
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-07 12:18:36 -0400

    Added size examples and carriage returns for sections Changes to be committed: 	modified:   Program.cs

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -9,15 +9,20 @@ int hexadecimalNotation = 0x_001E_8480;
 
 Console.WriteLine($"{decimalNotation == binaryNotation}");
 Console.WriteLine(
-    $"{decimalNotation == hexadecimalNotation}"
+    $"{decimalNotation == hexadecimalNotation}\n"
 );
 
 // Decimal output
 Console.WriteLine($"{decimalNotation:N0}");
 Console.WriteLine($"{binaryNotation:N0}");
-Console.WriteLine($"{hexadecimalNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}\n");
 
 // Hexadecimal output
 Console.WriteLine($"{decimalNotation:X}");
 Console.WriteLine($"{binaryNotation:X}");
-Console.WriteLine($"{hexadecimalNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}\n");
+
+// Demonstrate number sizes
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.\n");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range of {double.MinValue:N0} to {double.MaxValue:N0}.\n");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range of {decimal.MinValue:N0} to {decimal.MaxValue:N0}.\n");
