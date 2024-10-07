2c7103157bb7eb2dd05ea8f8e4fb3dfd7aa5b8e9
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-07 12:46:21 -0400

    Added variables Changes to be committed: 	modified:   Program.cs

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object height = 1.88; // store double in an object
+object name = "Amir"; // store string in an object
+Console.WriteLine($"{name} is {height} meters tall.");
+
+//int length1 = name.Length; // error: object does not contain .Length
+int length2 = ((string)name).Length; // cast name to a string
+Console.WriteLine($"{name} has {length2} characters");
