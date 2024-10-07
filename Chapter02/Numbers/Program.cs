e7a8366f129e0486e67b2f6b5d9fb4e180f35445
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-07 12:12:54 -0400

    Added numeric notation examples Changes to be committed: 	modified:   Program.cs

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿uint naturalNumber = 23;
+int integerNumber = -23;
+float realNumber = 2.3f;
+double anotherRealNumber = 2.3;
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine(
+    $"{decimalNotation == hexadecimalNotation}"
+);
+
+// Decimal output
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Hexadecimal output
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
