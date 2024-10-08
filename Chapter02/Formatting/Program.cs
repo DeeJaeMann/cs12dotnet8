1c2b1d7504d316d9e38c95ec460a2ad876112018
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-08 08:59:33 -0400

    Added more formatting examples Changes to be committed: 	modified:   Program.cs

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -14,3 +14,11 @@ string formatted = string.Format(
 // Used to illustrate concept
 //WriteToFile(formatted); // Writes the string to a file
 
+// Three parameter valuses can use namded arguments
+Console.WriteLine("{0} {1} lived in {2}.",
+    arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+
+// Four or more parameter values cannot used named arguments
+Console.WriteLine(
+    "{0} {1} lived in {2} and worked in the {3} team at {4}.",
+    "Roger", "Cevung", "Stockholm", "Education", "Optimizely");
