d2dc31a957669ab094c963512d3f6f9e9b6d58ab
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-08 08:56:06 -0400

    Added formatted string output Changes to be committed: 	modified:   Program.cs

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.3M;
+
+Console.WriteLine(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+string formatted = string.Format(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+// Used to illustrate concept
+//WriteToFile(formatted); // Writes the string to a file
+
