c9f19c9bc631c09989592252a4f46f2e03b4d23c
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-10-11 10:31:12 -0400

    Added unary increment assignements

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿#region Exploring unary operators
+
+int a = 3;
+int b = a++; // postfix operator - increment a after assignment
+WriteLine($"a is {a}, b is {b}"); // a is 4, b is 3
+
+int c = 3;
+int d = ++c; // prefix operator - increment c before assignment
+WriteLine($"c is {c}, d is {d}"); // c is 4, d is 4
+
+#endregion
