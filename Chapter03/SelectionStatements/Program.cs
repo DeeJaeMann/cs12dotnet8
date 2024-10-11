1835fff8159b30416f4c49702b4a9e2f3f362cde
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-10-11 11:42:08 -0400

    Added Switch Statements

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿#region If Statements
+
+string password = "ninja";
+
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 chars.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
+
+#endregion
+
+#region Pattern Matching
+
+object o = "3";
+int j = 4;
+
+if (o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiply!");
+}
+
+#endregion
+
+#region Switch Statments
+
+// Inclusive Lower bound but exclusive upper bound
+// Using Shared method because it is thread-safe
+int number = Random.Shared.Next(minValue: 1, maxValue: 7);
+WriteLine($"My random number is {number}");
+
+switch (number)
+{
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        break;
+    case 3:
+    case 4:
+        WriteLine("Three or four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+}
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
+
+#endregion
