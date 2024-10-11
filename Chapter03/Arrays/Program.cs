f4a7d7a905f0d3ff7b997b724a2d4e98c844789c
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-10-11 12:45:46 -0400

    Added Single Dimensional Arrays

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Single Dimensional Arrays
+
+string[] names;
+
+names = new string[4];
+
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+for (int i = 0; i < names.Length; i++)
+{
+    WriteLine($"{names[i]} is at position {i}.");
+}
+
+string[] names2 = { "Kate", "Jack", "Rebecca", "Tom" };
+
+for (int i = 0; i < names2.Length; i++)
+{
+    WriteLine($"{names2[i]} is at position {i}.");
+}
+
+#endregion
