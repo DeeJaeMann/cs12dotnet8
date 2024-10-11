50919738c780cd5d9fb17fd3444f673a74444f05
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-10-11 12:36:20 -0400

    Added for loops

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -7,3 +7,31 @@ while (x < 10)
 }
 
 #endregion
+#region Do Loop
+/*
+string? actualPassword = "Pa$$w0rd";
+string? password;
+
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+}
+while (password != actualPassword);
+
+WriteLine("Correct!");
+*/
+#endregion
+#region For Loop
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+for (int y = 1; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+
+#endregion
