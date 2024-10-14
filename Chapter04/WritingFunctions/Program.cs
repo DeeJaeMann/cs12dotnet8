161b07cd64a2f1cbb2466ff278dae32b025e7270
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 14:13:19 -0400

    Added more demonstrations Changes to be committed: 	modified:   Program.cs

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,10 +1,23 @@
 ﻿
+// Times table demonstration
 /*
 TimesTable(7);
 TimesTable(number: 7, size: 20);
 */
 
+// Functions with arguments demonstration
+// ConfigureConsole() has default args set
 ConfigureConsole();
 
 decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+
+ConfigureConsole(useComputerCulture: true);
+
+taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+
+ConfigureConsole(culture: "fr-FR");
+
+taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
 WriteLine($"You must pay {taxToPay:C} in tax.");
