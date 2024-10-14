d65b3361791aa47039b995adc285049159edde05
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 11:49:41 -0400

    Moved local function to class file Added project to solution Changes to be committed: 	modified:   ../Chapter04.sln 	new file:   Program.Functions.cs 	modified:   Program.cs

--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -4,8 +4,10 @@ WriteLine("* Top-level functions example");
 
 WhatsMyNamespace();
 
+/*
 void WhatsMyNamespace()
 {
     WriteLine("Namespace of Program class: {0}",
         arg0: typeof(Program).Namespace ?? "null");
 }
+*/
