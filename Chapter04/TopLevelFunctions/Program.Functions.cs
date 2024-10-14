367011d7e30866fcadb00e92889bf184a1d7a62e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 12:03:46 -0400

    Added comments to class Changes to be committed: 	modified:   TopLevelFunctions/Program.Functions.cs

--- Chapter04/TopLevelFunctions/Program.Functions.cs
+++ Chapter04/TopLevelFunctions/Program.Functions.cs
@@ -1,5 +1,9 @@
 using static System.Console;
 
+// Creating functions using this method merges them into automatically generated Program class
+// at the same level as Main()
+
+// Do NOT define a namespace here.
 partial class Program
 {
     static void WhatsMyNamespace()
