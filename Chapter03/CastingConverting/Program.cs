2d42a90c84f2f3bb43c14a41a2b24ebc92da59af
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 09:15:10 -0400

    Added converting statements Changes to be committed: 	modified:   Program.cs

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -1,4 +1,8 @@
-﻿int a = 10;
+﻿using static System.Convert;
+
+#region Casting
+
+int a = 10;
 double b = 1;
 WriteLine($"a is {a}, b is {b}");
 
@@ -18,3 +22,29 @@ WriteLine($"e is {e:N0}, f is {f:N0}");
 e = 5_000_000_000;
 f = (int)e; // Causes int overflow
 WriteLine($"e is {e:N0}, f is {f:N0}");
+
+WriteLine();
+
+WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--)
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+#endregion
+#region Converting
+
+WriteLine();
+
+double g = 9.8;
+int h = ToInt32(g); // Will round value
+WriteLine($"g is {g}, h is {h}");
+
+#endregion
+#region Rounding
+
+
+
+#endregion
