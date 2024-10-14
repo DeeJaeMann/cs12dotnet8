e3061390a3090e81c562783e2a1cdaf6359da4b9
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 09:06:04 -0400

    Added overflow examples Changes to be committed: 	modified:   Program.cs

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -5,3 +5,16 @@ WriteLine($"a is {a}, b is {b}");
 double c = 9.8;
 int d = (int)c;
 WriteLine($"c is {c}, d is {d}");
+
+long e = 10; // 64 bit
+int f = (int)e; // 32 bit
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+e = long.MaxValue;
+f = (int)e; // Value is too large 
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+//e = long.MaxValue;
+e = 5_000_000_000;
+f = (int)e; // Causes int overflow
+WriteLine($"e is {e:N0}, f is {f:N0}");
