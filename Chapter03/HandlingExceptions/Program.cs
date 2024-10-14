6a4fbf9939e168f03418e572b244c39859a4f9cf
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-14 10:13:12 -0400

    Added Try/Catch with specific exceptions Changes to be committed: 	modified:   HandlingExceptions.csproj 	modified:   Program.cs

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+
+try
+{
+    int age = int.Parse(input!); // null-forgiving operator, input will not be null
+    WriteLine($"You are {age} years old.");
+}
+// Empty catch block can safely complete program, however does not inform of exception
+//catch
+//{
+//}
+// Catch specific exceptions:
+catch (OverflowException)
+{
+    WriteLine("Your age is a valid number format but it is either too big or small.");
+}
+
+catch (FormatException)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
+
+// Catches all exceptions:
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing");
