553499f194a1fd362faffe813bd86440a222808e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-15 17:15:54 -0400

    Added HotReload demo instructions and message Changes to be committed: 	modified:   Program.cs

--- Chapter04/HotReloading/Program.cs
+++ Chapter04/HotReloading/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿/* Visual Studo 2022: run app, change message, click Hot Reload
+ * Visual Studio Code: run app using dotnet watch, change message
+ */
+
+while (true)
+{
+    WriteLine("Goodbye, Hot Reload!");
+    await Task.Delay(2000);
+}
