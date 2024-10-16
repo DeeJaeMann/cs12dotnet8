1d8d1a69807a7642c401bad2602de4277c18b520
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-16 09:32:04 -0400

    Set fields for bob object Output example to console Changes to be committed: 	modified:   Program.cs

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -3,4 +3,14 @@
 ConfigureConsole(); // Set current culture to US English
 
 Person bob = new();
+
+bob.Name = "Bob Smith";
+
+bob.Born = new DateTimeOffset(
+    year: 1965, month: 12, day: 22,
+    hour: 16, minute: 28, second: 0,
+    offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time
+
 WriteLine(bob);
+WriteLine(format: "{0} was born on {1:D}.", // Long date
+    arg0: bob.Name, arg1: bob.Born);
