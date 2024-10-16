28d3bed5636f49ade62f7c17306d614dc72011a3
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-16 09:20:39 -0400

    Set current culture Created bob object Output bob to console Changes to be committed: 	modified:   Program.cs

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared; // For Person
+
+ConfigureConsole(); // Set current culture to US English
+
+Person bob = new();
+WriteLine(bob);
