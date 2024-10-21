e2b577f3e83052d788139b09a1d071689b462ca4
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-21 13:21:10 -0400

    Added spouse handling methods Changes to be committed: 	modified:   ../PacktLibrary/Person.cs

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -8,6 +8,12 @@ public class Person
     public DateTimeOffset Born { get; set; }
     public List<Person> Children = new();
 
+    // Allow multiple spouses to be stored for a person
+    public List<Person> Spouses = new();
+
+    // read-only property
+    public bool Married => Spouses.Count > 0;
+
     #endregion
     #region Methods
 
@@ -20,6 +26,45 @@ public class Person
     {
         string term = Children.Count == 1 ? "child" : "children";
         WriteLine($"{Name} has {Children.Count} {term}.");
+    }
+
+    // static method to marry two people
+    public static void Marry(Person p1, Person p2)
+    {
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+
+        if (p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1))
+        {
+            throw new ArgumentException(
+                string.Format("{0} is already married to {1}.",
+                arg0: p1.Name, arg1: p2.Name));
+        }
+        p1.Spouses.Add(p2);
+        p2.Spouses.Add(p1);
+    }
+
+    // Instance method to marry another person
+    public void Marry(Person partner)
+    {
+        Marry(this, partner);
+    }
+
+    public void OutputSpouses()
+    {
+        if (Married)
+        {
+            string term = Spouses.Count == 1 ? "person" : "people";
+            WriteLine($"{Name} is married to {Spouses.Count} {term}:");
+            foreach (Person spouse in Spouses)
+            {
+                WriteLine($"  {spouse.Name}");
+            }
+        }
+        else
+        {
+            WriteLine($"{Name} is a singleton.");
+        }
     }
 
     #endregion
