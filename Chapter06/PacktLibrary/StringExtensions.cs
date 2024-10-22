ab96088915e14a03b81ba57e7bed48ca61d75d99
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-22 14:33:12 -0400

    Converted extension method to static

--- Chapter06/PacktLibrary/StringExtensions.cs
+++ Chapter06/PacktLibrary/StringExtensions.cs
@@ -2,9 +2,9 @@ using System.Text.RegularExpressions;
 
 namespace Packt.Shared;
 
-public class StringExtensions
+public static class StringExtensions
 {
-    public static bool IsValidEmail(string input)
+    public static bool IsValidEmail(this string input)
     {
         return Regex.IsMatch(input,
             @"[a-zA-zA-Z0-9\.-_]+@[a-zA-Z0-9\.-_]+");
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -241,3 +241,11 @@ WriteLine("{0} is a valid e-mail address: {1}",
 WriteLine("{0} is a valid e-mail address: {1}",
     arg0: email2,
     arg1: StringExtensions.IsValidEmail(email2));
+
+WriteLine("{0} is a valid e-mail address: {1}",
+    arg0: email1,
+    arg1: email1.IsValidEmail());
+
+WriteLine("{0} is a valid email address: {1}",
+    arg0: email2,
+    arg1: email2.IsValidEmail());
