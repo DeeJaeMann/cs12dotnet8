649aae126afeb2be8b678de048d598d183312873
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-22 05:11:35 -0400

    Added non-generic lookup via hashtable Changes to be committed: 	modified:   Program.cs

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -57,3 +57,22 @@ for (int i = 0; i < lamech.Children.Count; i++)
     arg0: lamech.Name, arg1: i,
     arg2: lamech.Children[i].Name);
 }
+
+WriteLine();
+
+// Non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2;
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
