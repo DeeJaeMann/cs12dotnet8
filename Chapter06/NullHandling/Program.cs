19cb5dbae1f52f550170f27d117665c992062165
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-22 11:17:48 -0400

    Added null operators demonstrations Changes to be committed: 	new file:   Address.cs 	modified:   Program.cs

--- Chapter06/NullHandling/Program.cs
+++ Chapter06/NullHandling/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Packt.Shared;
+
+int thisCannotBeNull = 4;
+//thisCannotBeNull = null; // CS0037 error
+WriteLine(thisCannotBeNull);
+
+int? thisCouldBeNull = null;
+
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+thisCouldBeNull = 7;
+
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+// actual type if int? is Nullable<int>
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 9;
+WriteLine(thisCouldAlsoBeNull);
+
+WriteLine();
+
+Address address = new(city: "London")
+{
+    Building = null,
+    Street = null!,
+    Region = "UK"
+};
+
+// Instead of throwing an exception, null is assigned ? suffix - null-conditional operator
+WriteLine(address.Building?.Length);
+// use is not null instead of != (!= may be overridden)
+if (address.Street is not null)
+{
+    WriteLine(address.Street.Length);
+}
+
+// Null-coalescing operator - ?? suffix
+WriteLine(address.Building?.Length ?? 10);
