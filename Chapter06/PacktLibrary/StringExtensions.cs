78c9fa4b71281f839cbcf581e4dbd52f48f54b0c
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-22 14:28:21 -0400

    Added StringExtensions with demonstration

---
+++ Chapter06/PacktLibrary/StringExtensions.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace Packt.Shared;
+
+public class StringExtensions
+{
+    public static bool IsValidEmail(string input)
+    {
+        return Regex.IsMatch(input,
+            @"[a-zA-zA-Z0-9\.-_]+@[a-zA-Z0-9\.-_]+");
+    }
+}
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -228,3 +228,16 @@ catch (PersonException ex)
 {
     WriteLine(ex.Message);
 }
+
+WriteLine();
+
+string email1 = "pamela@test.com";
+string email2 = "ian&test.com";
+
+WriteLine("{0} is a valid e-mail address: {1}",
+    arg0: email1,
+    arg1: StringExtensions.IsValidEmail(email1));
+
+WriteLine("{0} is a valid e-mail address: {1}",
+    arg0: email2,
+    arg1: StringExtensions.IsValidEmail(email2));
