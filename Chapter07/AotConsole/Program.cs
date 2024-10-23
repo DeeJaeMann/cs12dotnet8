808dd8c6f454494f2fb6f79fb30dfeb9fa759b1e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-23 10:16:23 -0400

    Added simple functionality for AoT publish Changes to be committed: 	modified:   Program.cs

--- Chapter07/AotConsole/Program.cs
+++ Chapter07/AotConsole/Program.cs
@@ -1,3 +1,8 @@
 ﻿using System.Globalization; // For CultureInfo
 
 WriteLine("This is an ahead-of-time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
+WriteLine("OS Version: {0}", Environment.OSVersion);
+
+Write("Press any key to exit.");
+ReadKey(intercept: true);
