996b6a86f705c44935caafa5c71c25008b34bd14
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-23 08:40:54 -0400

    Added comparison for using string vs String Changes to be committed: 	modified:   AssembliesAndNamespaces/AssembliesAndNamespaces.csproj 	modified:   AssembliesAndNamespaces/Program.cs

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq; // For XDocument
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
