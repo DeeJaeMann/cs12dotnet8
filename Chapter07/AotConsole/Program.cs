f1cb23139e7cd14922a10eb1ac450e2cd31b66ce
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-23 10:42:44 -0400

    Added example of incompatible code for AoT Changes to be committed: 	modified:   Program.cs

--- Chapter07/AotConsole/Program.cs
+++ Chapter07/AotConsole/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System.Globalization; // For CultureInfo
+using System.Reflection; // For AssemblyName
+using System.Reflection.Emit; // For AssemblyBuilder
+
+// Cannot use this, will break during AoT Compilation
+// Requires dynamic code
+/*
+AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly(
+    new AssemblyName("MyAssembly"), AssemblyBuilderAccess.Run);
+*/
 
 WriteLine("This is an ahead-of-time (AOT) compiled console app.");
 WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
