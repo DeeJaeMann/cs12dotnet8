e1fc3c4994a4ad86880f5fff2f5e393d1ffbc723
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-25 10:22:30 -0400

    Added string searching Added string comparison Changes to be committed: 	modified:   Program.cs

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,4 +1,8 @@
-﻿string city = "London";
+﻿using System.Globalization; // For CultureInfo
+
+OutputEncoding = System.Text.Encoding.UTF8; // Enable Euro symbol
+
+string city = "London";
 WriteLine($"{city} is {city.Length} characters long.");
 WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
 
@@ -47,3 +51,26 @@ WriteLine($"Original: {fullName}");
 WriteLine($"Swapped: {firstName} {lastName}");
 
 WriteLine();
+
+string company = "Microsoft";
+WriteLine($"Text: {company}");
+WriteLine("Starts with M: {0}, contans an N: {1}",
+    arg0: company.StartsWith("M"),
+    arg1: company.Contains("N"));
+
+WriteLine();
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+string text1 = "Mark";
+string text2 = "MARK";
+
+WriteLine($"text1: {text1}, text2: {text2}");
+
+WriteLine("Compare: {0}.", string.Compare(text1, text2));
+
+WriteLine("Compare (ignoreCase): {0}",
+    string.Compare(text1, text2, ignoreCase: true));
+
+WriteLine("Compare (InvariantCultureIgnoreCase): {0}.",
+    string.Compare(text1, text2, StringComparison.InvariantCultureIgnoreCase));
