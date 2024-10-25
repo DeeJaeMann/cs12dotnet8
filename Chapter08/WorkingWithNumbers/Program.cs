4c96a45855393feb0deda57a7ff09cae4a8f5488
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-25 09:35:01 -0400

    Added ulong and BigInteger demonstration Added project to solution Changes to be committed: 	modified:   ../Chapter08.sln 	modified:   Program.cs 	modified:   WorkingWithNumbers.csproj

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics; // For BigInteger
+
+const int width = 40;
+
+WriteLine("ulong.MaxValue vs a 30-digit BigInteger");
+WriteLine(new string('-', width));
+
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}");
