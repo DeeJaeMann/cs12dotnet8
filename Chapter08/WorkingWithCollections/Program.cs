263a00903337da274883c5187c357c8c6c732920
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-25 12:52:28 -0400

    Added Dictionary demonstration Changes to be committed: 	modified:   Program.cs

--- Chapter08/WorkingWithCollections/Program.cs
+++ Chapter08/WorkingWithCollections/Program.cs
@@ -1,4 +1,7 @@
-﻿// Simple syntax for creating a list and adding 3 items
+﻿//Define alias for a dictionary with string key and string value
+using StringDictionary = System.Collections.Generic.Dictionary<string, string>;
+
+// Simple syntax for creating a list and adding 3 items
 List<string> cities = new();
 cities.Add("London");
 cities.Add("Paris");
@@ -22,3 +25,44 @@ OutputCollection("After inserting Sydney at index 0", cities);
 cities.RemoveAt(1);
 cities.Remove("Milan");
 OutputCollection("After removing two cities", cities);
+
+WriteLine();
+
+// Declare dictionary without alias
+// Dictionary<string, string> keywords = new();
+
+// Using the alias
+StringDictionary keywords = new();
+
+keywords.Add(key: "int", value: "32-bit integer data type");
+keywords.Add("long", "64-bit integer data type");
+keywords.Add("float", "Single precision floating point number");
+
+/* Alternative syntax, compiler convers to Add method calls
+Dictionary<string, string> = new()
+{
+    { "int", "32-bit integer data type" },
+    { "long", "64-bit integer data type" },
+    { "float", "Single precision floating point number" },
+}; */
+
+/* Alternative syntax, compiler converts to Add method calls
+Dictionary<string, string> = new()
+{
+    ["int"] = "32-bit integer data type",
+    ["long"] = "64-bit integer data type",
+    ["float"] = "Single precision floating point number",
+}; */
+
+OutputCollection("Dictionary keys", keywords.Keys);
+OutputCollection("Dictionary values", keywords.Values);
+
+WriteLine("Keywords and their definitions:");
+foreach (KeyValuePair<string, string> item in keywords)
+{
+    WriteLine($"  {item.Key}: {item.Value}");
+}
+
+// Look up value using key
+string key = "long";
+WriteLine($"The definition of {key} is {keywords[key]}");
