fcdc70966c8c135beee8cfab0328ac78d4479c86
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-25 12:40:19 -0400

    Added usage demonstration Changes to be committed: 	modified:   Program.cs

--- Chapter08/WorkingWithCollections/Program.cs
+++ Chapter08/WorkingWithCollections/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Simple syntax for creating a list and adding 3 items
+List<string> cities = new();
+cities.Add("London");
+cities.Add("Paris");
+cities.Add("Milan");
+
+/* Alternative syntax - converted into add methods by compiler
+List<string> cities = new()
+{ "London", "Paris", "Milan" }; */
+
+/* Alternative syntax - passes an array of string values to AddRange method
+List<string> cities = new();
+cities.AddRange(new[] { "London", "Paris", "Milan" }); */
+
+OutputCollection("Initial list", cities);
+WriteLine($"The first city is {cities[0]}.");
+WriteLine($"The last city is {cities[cities.Count - 1]}.");
+
+cities.Insert(0, "Sydney");
+OutputCollection("After inserting Sydney at index 0", cities);
+
+cities.RemoveAt(1);
+cities.Remove("Milan");
+OutputCollection("After removing two cities", cities);
