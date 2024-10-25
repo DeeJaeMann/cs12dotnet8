c39eb2a780cc0c9ff179457457835c0227a952d1
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-25 11:19:26 -0400

    Added simple RegEx Changes to be committed: 	modified:   Program.cs

--- Chapter08/WorkingWithRegEx/Program.cs
+++ Chapter08/WorkingWithRegEx/Program.cs
@@ -1,1 +1,8 @@
 ﻿using System.Text.RegularExpressions;
+
+Write("Enter your age: ");
+string input = ReadLine()!;
+Regex ageChecker = new(@"^\d+$");
+WriteLine(ageChecker.IsMatch(input) ? "Thank you!" :
+    $"This is not a valid age: {input}");
+
