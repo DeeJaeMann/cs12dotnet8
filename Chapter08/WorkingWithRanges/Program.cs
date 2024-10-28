c57216a3d8d9495eda6da77436cc2f753a034e92
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-28 12:43:00 -0400

    Added Span, Index and Range examples

--- Chapter08/WorkingWithRanges/Program.cs
+++ Chapter08/WorkingWithRanges/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string name = "Samantha Jones";
 
-Console.WriteLine("Hello, World!");
+// Get lengths of first and last names
+int lengthOfFirst = name.IndexOf(' ');
+int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+// Using substring
+// creates copy
+string firstName = name.Substring(startIndex: 0, length: lengthOfFirst);
+
+string lastName = name.Substring(startIndex: name.Length - lengthOfLast, length: lengthOfLast);
+
+WriteLine($"First: {firstName}, Last: {lastName}");
+
+// Using spans
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = nameAsSpan[0..lengthOfFirst]; // From 0 to lengthOfFirst
+ReadOnlySpan<char> lastNameSpan = nameAsSpan[^lengthOfLast..]; // From end to end
+
+WriteLine($"First: {firstNameSpan}, Last: {lastNameSpan}");
