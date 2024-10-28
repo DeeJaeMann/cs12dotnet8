63e3cb7d5fe606f86f02e63eaf1f104f321b9358
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-28 16:56:06 -0400

    Added file info demo

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -139,4 +139,14 @@ WriteLine($"File Extension: {GetExtension(textFile)}");
 WriteLine($"Random File Name: {GetRandomFileName()}");
 WriteLine($"Temporary File Name: {GetTempFileName()}");
 
+WriteLine();
+
+SectionTitle("Getting file information");
+
+FileInfo info = new(backupFile);
+WriteLine($"{backupFile}:");
+WriteLine($"  Contains {info.Length} bytes");
+WriteLine($"  Last accessed: {info.LastAccessTime}");
+WriteLine($"  Has readonly set to {info.IsReadOnly}");
+
 #endregion
