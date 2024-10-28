2b7e56c2946dfb2ba45d3c90b3ac3d6603ef2b21
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-28 17:23:48 -0400

    Added text steam demo

--- Chapter09/WorkingWithStreams/Program.cs
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Packt.Shared;
 
-Console.WriteLine("Hello, World!");
+SectionTitle("Writing to text streams");
+
+string textFile = Combine(CurrentDirectory, "streams.txt");
+StreamWriter text = File.CreateText(textFile);
+
+foreach (string item in Viper.Callsigns)
+{
+    text.WriteLine(item);
+}
+text.Close();
+
+OutputFileInfo(textFile);
