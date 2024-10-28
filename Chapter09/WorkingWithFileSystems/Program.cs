3c137ae4535e35d6b0277c119e84aa344e5f00aa
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-28 16:49:53 -0400

    Added file read/write demo

--- Chapter09/WorkingWithFileSystems/Program.cs
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -88,4 +88,45 @@ WriteLine("Deleting it...");
 Delete(newFolder, recursive: true);
 WriteLine($"Does it exist? {Path.Exists(newFolder)}");
 
+WriteLine();
+
+SectionTitle("Managing files");
+
+// Define dir path to output files starting in user's dir
+string dir = Combine(
+    GetFolderPath(SpecialFolder.Personal), "OutputFiles");
+
+CreateDirectory(dir);
+
+string textFile = Combine(dir, "Dummy.txt");
+string backupFile = Combine(dir, "Dummy.bak");
+WriteLine($"Working with: {textFile}");
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Create new text file and write a line to it
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Hello C#!");
+textWriter.Close();
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Copy file and overwrite if already exists
+File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+
+WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+
+Write("Confirm the file exists, and then press any key.");
+ReadKey(intercept: true);
+
+// Delete the file
+File.Delete(textFile);
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Read from text file backup
+WriteLine($"Reading contents of {backupFile}:");
+StreamReader textReader = File.OpenText(backupFile);
+WriteLine(textReader.ReadToEnd());
+textReader.Close();
+
+WriteLine();
+
 #endregion
