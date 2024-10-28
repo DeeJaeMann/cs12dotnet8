d04abb4b6d1eaa8fa2c7b98d85d17450be0a94e6
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-28 17:44:25 -0400

    Added XML Stream demo

--- Chapter09/WorkingWithStreams/Program.cs
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -1,4 +1,5 @@
-﻿using Packt.Shared;
+﻿using System.Xml;
+using Packt.Shared;
 
 SectionTitle("Writing to text streams");
 
@@ -12,3 +13,51 @@ foreach (string item in Viper.Callsigns)
 text.Close();
 
 OutputFileInfo(textFile);
+
+WriteLine();
+
+SectionTitle("Writing to XML streams:");
+
+string xmlFile = Combine(CurrentDirectory, "streams.xml");
+FileStream? xmlFileStream = null;
+XmlWriter? xml = null;
+
+try
+{
+    xmlFileStream = File.Create(xmlFile);
+
+    // Wrap file stream in XML writer helper and tell it to automatically indent nested elements
+    xml = XmlWriter.Create(xmlFileStream,
+        new XmlWriterSettings { Indent = true });
+
+    xml.WriteStartDocument();
+    xml.WriteStartElement("callsigns");
+
+    foreach (string item in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", item);
+    }
+
+    xml.WriteEndElement();
+}
+catch (Exception ex)
+{
+    // If path doesn't exist exception will be caught
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+finally
+{
+    if (xml is not null)
+    {
+        xml.Close();
+        WriteLine("The XML writer's unmanaged resources have been disposed.");
+    }
+
+    if (xmlFileStream is not null)
+    {
+        xmlFileStream.Close();
+        WriteLine("The file stream's unmanaged resources have been disposed.");
+    }
+}
+
+OutputFileInfo(xmlFile);
