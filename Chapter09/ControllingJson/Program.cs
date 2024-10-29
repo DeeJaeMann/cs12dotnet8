d6fb9b9a7469a9c08838661caab73f08e963f132
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-29 11:07:07 -0400

    Added JSON Processing demo

--- Chapter09/ControllingJson/Program.cs
+++ Chapter09/ControllingJson/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using Microsoft.VisualBasic; // For JsonSerializer
+using Packt.Shared;
 
-Console.WriteLine("Hello, World!");
+Book csharpBook = new(title:
+    "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J Price",
+    PublishDate = new(year: 2023, month: 11, day: 14),
+    Pages = 823,
+    Created = DateTimeOffset.UtcNow,
+};
+
+JsonSerializerOptions options = new()
+{
+    IncludeFields = true,
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+
+string path = Combine(CurrentDirectory, "book.json");
+
+using (Stream fileStream = File.Create(path))
+{
+    JsonSerializer.Serialize(
+        utf8Json: fileStream, value: csharpBook, options);
+}
+
+WriteLine("**** File Info ****");
+WriteLine($"File: {GetFileName(path)}");
+WriteLine($"Path: {GetDirectoryName(path)}");
+WriteLine($"Size: {new FileInfo(path).Length:N0} bytes");
+WriteLine("/----------");
+WriteLine(File.ReadAllText(path));
+WriteLine("----------/");
