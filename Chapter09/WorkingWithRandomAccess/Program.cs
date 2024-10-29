7f65af76f882a9fa3c597c0e401cc23333f97b5a
Dee J. Mann <dee.j.mann@hotmail.com>
2024-10-29 09:14:34 -0400

    Added reading/writing with random access handles demo

--- Chapter09/WorkingWithRandomAccess/Program.cs
+++ Chapter09/WorkingWithRandomAccess/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Win32.SafeHandles; // For SafeFileHandle
+using System.Text; // For Encoding
 
-Console.WriteLine("Hello, World!");
+using SafeFileHandle handle = File.OpenHandle(path: "coffee.txt",
+    mode: FileMode.OpenOrCreate,
+    access: FileAccess.ReadWrite);
+
+// Write string encoded as byte array
+// Store in read-only memory buffer to file
+string message = "Cafe $4.39";
+ReadOnlyMemory<byte> buffer = new(Encoding.UTF8.GetBytes(message));
+await RandomAccess.WriteAsync(handle, buffer, fileOffset: 0);
+
+// Read from file, get length of file, allocate memory buffer for contents and read file
+long length = RandomAccess.GetLength(handle);
+Memory<byte> contentBytes = new(new byte[length]);
+await RandomAccess.ReadAsync(handle, contentBytes, fileOffset: 0);
+string content = Encoding.UTF8.GetString(contentBytes.ToArray());
+WriteLine($"Content of file: {content}");
