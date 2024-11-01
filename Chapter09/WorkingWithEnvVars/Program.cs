7305de3b0b7c7e52e390418192f9ac19afe8a45c
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-01 17:26:08 -0400

    Added more env variable demos

--- Chapter09/WorkingWithEnvVars/Program.cs
+++ Chapter09/WorkingWithEnvVars/Program.cs
@@ -11,3 +11,28 @@ SectionTitle("Reading all environment variables for user");
 IDictionary varsUser = GetEnvironmentVariables(
     EnvironmentVariableTarget.User);
 DictionaryToTable(varsUser);
+
+WriteLine();
+
+string myComputer = "My username is %LOGNAME%. My CPU is %MACHTYPE%";
+
+WriteLine(ExpandEnvironmentVariables(myComputer));
+
+string passwordKey = "MY_PASSWORD";
+
+SetEnvironmentVariable(passwordKey, "Pa$$w0rd");
+string? password = GetEnvironmentVariable(passwordKey);
+WriteLine($"{passwordKey}: {password}");
+
+string secretKey = "MY_SECRET";
+string? secret = GetEnvironmentVariable(secretKey,
+    EnvironmentVariableTarget.Process);
+WriteLine($"Process - {secretKey}: {secret}");
+
+secret = GetEnvironmentVariable(secretKey,
+    EnvironmentVariableTarget.Machine);
+WriteLine($"Machine - {secretKey}: {secret}");
+
+secret = GetEnvironmentVariable(secretKey,
+    EnvironmentVariableTarget.User);
+WriteLine($"User - {secretKey}: {secret}");    
