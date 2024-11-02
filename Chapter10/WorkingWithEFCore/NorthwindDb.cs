62711119d781e5c3d48d95532926fa63c8f449db
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-02 07:47:31 -0400

    Added table mapping properties and Fluent API demo

--- Chapter10/WorkingWithEFCore/NorthwindDb.cs
+++ Chapter10/WorkingWithEFCore/NorthwindDb.cs
@@ -5,6 +5,9 @@ namespace Northwind.EntityModels;
 // manages interactions with Northwind database
 public class NorthwindDb : DbContext
 {
+    // These two properties map to tables in the database
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string databaseFile = "Northwind.db";
@@ -12,5 +15,23 @@ public class NorthwindDb : DbContext
         string connectionString = $"Data Source={path}";
         WriteLine($"Connection: {connectionString}");
         optionsBuilder.UseSqlite(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Example of using Fluent API instead of attributes
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired() // NOT NULL
+            .HasMaxLength(15);
+        
+        // SQLite-specific configuration
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            // To 'fix' the lack of decimal support in SQLite
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
     }
 }
