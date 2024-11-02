0a4c43bc126c0afea1ac55879cacecf896e36406
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-02 07:39:17 -0400

    Defined Product class

--- Chapter10/Northwind.EntityModels/Product.cs
+++ Chapter10/Northwind.EntityModels/Product.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations; // For [Required]
+using System.ComponentModel.DataAnnotations.Schema; // For [Column]
+
 namespace Northwind.EntityModels;
 
 public class Product
 {
+    public int ProductId { get; set; } // Primary key
+
+    [Required] [StringLength(40)] public string ProductName { get; set; } = null!;
     
+    // Property name is different from column name
+    [Column("UnitPrice", TypeName = "money")]
+    public decimal? Cost { get; set; }
+    
+    [Column("UnitsInStock")]
+    public short? Stock { get; set; }
+    
+    public bool Discountinued { get; set; }
+    
+    // These two properties define the foreign key relationship
+    // to the Categories table
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; } = null!;
 }
