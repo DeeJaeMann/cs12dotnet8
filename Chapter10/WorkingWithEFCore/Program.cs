1944ecacc882c966b0d249be3a25bd82d1952f49
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-02 07:01:22 -0400

    Added DB Connection demo

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Northwind.EntityModels;
 
-Console.WriteLine("Hello, World!");
+using NorthwindDb db = new();
+WriteLine($"Provider: {db.Database.ProviderName}");
