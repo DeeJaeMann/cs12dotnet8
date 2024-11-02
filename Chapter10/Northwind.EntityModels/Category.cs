169e2cb08f64ed539ff7b662c37c4a11852094d5
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-02 07:30:07 -0400

    Defined Category class

--- Chapter10/Northwind.EntityModels/Category.cs
+++ Chapter10/Northwind.EntityModels/Category.cs
@@ -1,5 +1,20 @@
-﻿namespace Northwind.EntityModels;
+﻿using System.ComponentModel.DataAnnotations.Schema; // For [Column]
+
+namespace Northwind.EntityModels;
 
 public class Category
 {
+    // These properties map to columns in the db
+    public int CategoryId { get; set; } // Primary key
+    public string CategoryName { get; set; } = null!;
+    
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+    
+    // Define navigation property for related rows
+    public virtual ICollection<Product> Products { get; set; }
+    // To enable developers to add products to a Category, we must
+    // initialize the navigation property to an empty collection.
+    // This also avoids an exception if we get a member like Count
+        = new HashSet<Product>();
 }
