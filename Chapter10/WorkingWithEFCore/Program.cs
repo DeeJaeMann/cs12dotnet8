61507e5698fe61d29dcf92ce696e984e5013f363
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-05 10:48:02 -0500

    Added delete method demo

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -27,6 +27,7 @@ if (resultAdd.affected == 1)
 ListProducts(productIdsToHighlight: new[] { resultAdd.productId });
 */
 
+/*
 var resultUpdate = IncreaseProductPrice(
     productNameStartsWith: "Bob", amount: 20M);
 
@@ -36,3 +37,16 @@ if (resultUpdate.affected == 1)
 }
 
 ListProducts(productIdsToHighlight: new[] { resultUpdate.productId });
+*/
+
+WriteLine("About to delete all products whose name starts with Bob.");
+Write("Press Enter to continue or any other key to exit: ");
+if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
+{
+    int deleted = DeleteProducts(productNameStartsWith: "Bob");
+    WriteLine($"{deleted} product(s) were deleted.");
+}
+else
+{
+    WriteLine("Delete was canceled.");
+}
