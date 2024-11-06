801f86982564bf85fc4ef8ae53f632eadd1817ef
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-06 11:48:10 -0500

    Defined class logic for each class

--- Chapter10/CoursesAndStudents/Course.cs
+++ Chapter10/CoursesAndStudents/Course.cs
@@ -1,6 +1,16 @@
-namespace CoursesAndStudents;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.Shared;
 
 public class Course
 {
+    public int CourseId { get; set; }
     
+    [Required]
+    [StringLength(60)] // For SQL Server - SQLite doesn't honor this
+    [Column(TypeName = "text(60)")] // For SQLite
+    public string? Title { get; set; }
+    
+    public ICollection<Student>? Students { get; set; }
 }
