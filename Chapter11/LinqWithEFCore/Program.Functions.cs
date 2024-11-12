cae0794141b6b71de0c2fb40ea36d615793443b2
Dee J. Mann <dee.j.mann@hotmail.com>
2024-11-12 10:46:05 -0500

    Added projected results improvement

--- Chapter11/LinqWithEFCore/Program.Functions.cs
+++ Chapter11/LinqWithEFCore/Program.Functions.cs
@@ -12,14 +12,32 @@ partial class Program
         DbSet<Product> allProducts = db.Products;
         IQueryable<Product> filteredProducts =
             allProducts.Where(product => product.UnitPrice < 10M);
-
+        
         IOrderedQueryable<Product> sortedAndFilteredProducts =
             filteredProducts.OrderByDescending(product => product.UnitPrice);
         
+        // Projecting the results
+        var projectedProducts = sortedAndFilteredProducts
+            .Select(product => new
+            {
+                product.ProductId,
+                product.ProductName,
+                product.UnitPrice,
+            });
+        
         WriteLine("Products that cost less than $10:");
-        WriteLine(sortedAndFilteredProducts.ToQueryString());
+        //WriteLine(sortedAndFilteredProducts.ToQueryString());
+        WriteLine(projectedProducts.ToQueryString());
 
+        // Using previous method - Queried all columns
+        /*
         foreach (Product p in sortedAndFilteredProducts)
+        {
+            WriteLine("{0}: {1} costs {2:$#,##0.00}",
+                p.ProductId, p.ProductName, p.UnitPrice);
+        }
+        */
+        foreach (var p in projectedProducts)
         {
             WriteLine("{0}: {1} costs {2:$#,##0.00}",
                 p.ProductId, p.ProductName, p.UnitPrice);
