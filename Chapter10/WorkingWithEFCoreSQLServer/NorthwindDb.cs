a878fc2ba0ad0e9fb8f071dfcdfcb92e95733937
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-14 10:13:36 -0500

    Added WorkingWithEFCoreSQLServer project Added NuGet packages Added NorthwindDb class/model

---
+++ Chapter10/WorkingWithEFCoreSQLServer/NorthwindDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient; // For SqlConnectionStringBuilder
+using Microsoft.EntityFrameworkCore; // For DbContext
+
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        SqlConnectionStringBuilder builder = new();
+
+        builder.DataSource = "."; // "ServerName\InstanceName" ie. @".\sqlexpress"
+        builder.InitialCatalog = "Northwind";
+        builder.Encrypt = true;
+        builder.TrustServerCertificate = true;
+        builder.MultipleActiveResultSets = true;
+        builder.ConnectTimeout = 3; // Default is 15 seconds
+
+        // If using Windows Integrated authentication
+        builder.IntegratedSecurity = true;
+
+        // If using SQL Server authentication
+        // builder.UserId = Environment.GetEnvironmentVariable("MY_SQL_USR");
+        // builder.Password = Environment.GetEnvironmentVariable("MY_SQL_PWD");
+
+        string? connectionString = builder.ConnectionString;
+        WriteLine($"Connection: {connectionString}");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
