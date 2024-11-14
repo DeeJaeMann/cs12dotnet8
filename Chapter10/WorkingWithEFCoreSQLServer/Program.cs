66aac9bfe8ce9f084c340eb203adfd5e0137fde9
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-14 10:22:53 -0500

    Added project reference to WorkingWithEFCoreSQLServer

--- Chapter10/WorkingWithEFCoreSQLServer/Program.cs
+++ Chapter10/WorkingWithEFCoreSQLServer/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Northwind.EntityModels; // For NorthwindDb
+
+NorthwindDb db = new();
+WriteLine($"Provider: {db.Database.ProviderName}");
