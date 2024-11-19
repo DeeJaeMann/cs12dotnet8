dd529a302437d7ec39677114dc9ab3e304158857
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-19 08:31:38 -0500

    Added LinqWithEFCoreSQLServer console project and defined models

---
+++ Chapter11/LinqWithEFCoreSQLServer/EntityModels/NorthwindDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient; // For SqlConnectionStringBuilder
+using Microsoft.EntityFrameworkCore; // For DbContext, DbSet<t>
+
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        SqlConnectionStringBuilder builder = new();
+
+        builder.DataSource = ".";
+        builder.InitialCatalog = "Northwind";
+        builder.IntegratedSecurity = true;
+        builder.Encrypt = true;
+        builder.TrustServerCertificate = true;
+        builder.MultipleActiveResultSets = true;
+
+        string connection = builder.ConnectionString;
+        WriteLine($"SQL Server connection: {connection}");
+
+        optionsBuilder.UseSqlServer(connection);
+    }
+
+}
---
+++ Chapter11/LinqWithEFCoreSQLServer/EntityModels/Product.cs
@@ -0,0 +1,31 @@
+﻿// For [Required] and [StringLength]
+using System.ComponentModel.DataAnnotations;
+
+// For [Column]
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.EntityModels;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public string ProductName { get; set; } = null!;
+
+    public int? SupplierId { get; set; }
+    public int? CategoryId { get; set; }
+
+    [StringLength(20)]
+    public string? QuantityPerUnit { get; set; }
+
+    // Required for SQL Server Provider
+    [Column(TypeName = "money")]
+    public decimal? UnitPrice { get; set; }
+
+    public short? UnitsInStock { get; set; }
+    public short? UnitsOnOrder { get; set; }
+    public short? ReorderLevel { get; set; }
+    public bool Discontinued { get; set; }
+}
