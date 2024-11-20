ff9fbdd4fa753526ddcaf97282c379a5e84d4144
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-20 08:26:48 -0500

    Added pagination

--- Chapter11/LinqWithEFCoreSQLServer/Program.Functions.cs
+++ Chapter11/LinqWithEFCoreSQLServer/Program.Functions.cs
@@ -1,5 +1,6 @@
 ﻿using Northwind.EntityModels; // For NorthwindDb, Category, Product
-using Microsoft.EntityFrameworkCore; // For DbSet<T>
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices; // For DbSet<T>
 
 partial class Program
 {
@@ -179,5 +180,71 @@ partial class Program
         WriteLine($"{"Average unit price:",-25} {db.Products.Average(p => p.UnitPrice),10:$#,##0.00}");
 
         WriteLine($"{"Value of units in stock:",-25} {db.Products.Sum(p => p.UnitPrice * p.UnitsInStock),10:$#,##0.00}");
+    }
+
+    private static void OutputTableOfProducts(Product[] products, int currentPage, int totalPages)
+    {
+        string line = new('-', count: 73);
+        string lineHalf = new('-', count: 30);
+
+        WriteLine(line);
+        WriteLine("{0,4} {1,-40} {2,12} {3,-15}",
+            "ID", "Product Name", "Unit Price", "Discontinued");
+        WriteLine(line);
+
+        foreach (Product p in products)
+        {
+            WriteLine("{0,4} {1,-40} {2,12:C} {3,-15}",
+                p.ProductId, p.ProductName, p.UnitPrice, p.Discontinued);
+        }
+        WriteLine("{0} Page {1} of {2} {3}",
+            lineHalf, currentPage + 1, totalPages + 1, lineHalf);
+    }
+
+    private static void OutputPageOfProducts(IQueryable<Product> products, int pageSize, int currentPage, int totalPages)
+    {
+        // Order data before skipping and taking to ensure the data is not randomly sorted in each page
+        var pagingQuery = products.OrderBy(p => p.ProductId)
+            .Skip(currentPage * pageSize).Take(pageSize);
+
+        Clear(); // Clear console
+
+        SectionTitle(pagingQuery.ToQueryString());
+
+        OutputTableOfProducts(pagingQuery.ToArray(), currentPage, totalPages);
+    }
+
+    private static void PagingProducts()
+    {
+        SectionTitle("Paging products");
+
+        using NorthwindDb db = new();
+
+        int pageSize = 10;
+        int currentPage = 0;
+        int productCount = db.Products.Count();
+        int totalPages = productCount / pageSize;
+
+        while (true)
+        {
+            OutputPageOfProducts(db.Products, pageSize, currentPage, totalPages);
+            Write("Press <- to page back, press -> to page forward, any key to exit.");
+            ConsoleKey key = ReadKey().Key;
+
+            if (key == ConsoleKey.LeftArrow)
+            {
+                currentPage = currentPage == 0 ? totalPages : currentPage - 1;
+            }
+            else if (key == ConsoleKey.RightArrow )
+            {
+                currentPage = currentPage == totalPages ? 0 : currentPage + 1;
+            }
+            else
+            {
+                break;
+            }
+
+            WriteLine();
+        }
     }
 }
