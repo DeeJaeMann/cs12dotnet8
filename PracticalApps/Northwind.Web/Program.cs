8fd82ee3887bb52769a2895a82cbe332f3d35182
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-21 09:21:17 -0500

    Added static page Added routing Added environment endpoint

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,7 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region Configure HTTP pipeline and routes
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/hello", () => $"Environment is {app.Environment.EnvironmentName}");
+
+#endregion
 
 // Start the web server, host the website, and wait for requests
 app.Run(); // Thread-blocking call
