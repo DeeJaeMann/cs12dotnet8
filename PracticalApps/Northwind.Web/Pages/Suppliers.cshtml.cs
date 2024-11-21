865adfef3188c9fcb27d0957eaa52ab340186e0a
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-21 11:17:59 -0500

    Added form to insert company

--- PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
+++ PracticalApps/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; // For [BindProperty], IActionResult
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Northwind.EntityModels; // For NortwindContext
 
@@ -11,6 +11,9 @@ namespace Northwind.Web.Pages
         // For static example
         //public IEnumerable<string>? Suppliers { get; set; }
         public IEnumerable<Supplier>? Suppliers { get; set; }
+
+        [BindProperty]
+        public Supplier? Supplier { get; set; }
 
         public SuppliersModel(NorthwindContext db)
         {
@@ -30,6 +33,20 @@ namespace Northwind.Web.Pages
             Suppliers = _db.Suppliers
                 .OrderBy(c => c.Country)
                 .ThenBy(c => c.CompanyName);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Supplier is not null && ModelState.IsValid)
+            {
+                _db.Suppliers.Add(Supplier);
+                _db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            else
+            {
+                return Page(); // Return to original page
+            }
         }
     }
 }
