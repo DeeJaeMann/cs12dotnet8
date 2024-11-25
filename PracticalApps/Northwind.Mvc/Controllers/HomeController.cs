a472e52fe7d386d561142ffb5fbf2dd8b73a40f4
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-25 11:53:58 -0500

    Modified Index Action method to add db data

--- PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
+++ PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -1,25 +1,37 @@
 using System.Diagnostics; // For Activity
 using Microsoft.AspNetCore.Mvc; // For Controller, IActionResult
 using Northwind.Mvc.Models; // For ErrorViewModel
+using Northwind.EntityModels; // For NortwindContext
 
 namespace Northwind.Mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NorthwindContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NorthwindContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+            // Logging demo
+            /*
             _logger.LogError("This is a serious error (not really!)");
             _logger.LogWarning("This is your first warning!");
             _logger.LogWarning("Second warning!");
             _logger.LogInformation("I am in the Index method of the HomeController.");
-            return View();
+            */
+            HomeIndexViewModel model = new
+                (
+                VisitorCount: Random.Shared.Next(1, 1001),
+                Categories: _db.Categories.ToList(),
+                Products: _db.Products.ToList()
+                );
+            return View(model); // Pass the model to the view
         }
 
         public IActionResult Privacy()
