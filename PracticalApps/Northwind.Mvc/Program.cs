7b4e441e02a4a8a487b5112db3ca9e34910d9ca0
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-25 11:46:24 -0500

    Added connection to Northwind Db

--- PracticalApps/Northwind.Mvc/Program.cs
+++ PracticalApps/Northwind.Mvc/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity; // For IdentityUser
 using Microsoft.EntityFrameworkCore; // For UseSqlServer
 using Northwind.Mvc.Data; // For ApplicationDbContext
+using Northwind.EntityModels; // For AddNorthwindContext
 #endregion
 
 #region Configure the host web server including services
@@ -16,6 +17,23 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+// For SQLite
+//builder.Services.AddNorthwindContext();
+
+// For SQL Server
+string? sqlServerConnection = builder.Configuration.GetConnectionString("NorthwindConnection");
+
+if (sqlServerConnection is null)
+{
+    Console.WriteLine("SQL Server database connection string is missing!");
+}
+else
+{
+    Microsoft.Data.SqlClient.SqlConnectionStringBuilder sql = new(sqlServerConnection);
+    sql.IntegratedSecurity = true;
+    builder.Services.AddNorthwindContext(sql.ConnectionString);
+}
 
 var app = builder.Build();
 #endregion
