b2f5ebacbdd60f0a960e90ed5497f2b7d91355bb
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-25 12:35:19 -0500

    Added ProductDetails display and logic

--- PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
+++ PracticalApps/Northwind.Mvc/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics; // For Activity
 using Microsoft.AspNetCore.Mvc; // For Controller, IActionResult
 using Northwind.Mvc.Models; // For ErrorViewModel
 using Northwind.EntityModels; // For NortwindContext
+using Microsoft.EntityFrameworkCore; // For Include
 
 namespace Northwind.Mvc.Controllers
 {
@@ -43,6 +44,24 @@ namespace Northwind.Mvc.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ProductDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
+            }
+
+            Product? model = _db.Products.Include(p => p.Category)
+                .SingleOrDefault(p => p.ProductId == id);
+
+            if (model is null)
+            {
+                return NotFound($"ProductId {id} not found.");
+            }
+
+            return View(model); // Pass model to view then return result
         }
     }
 }
