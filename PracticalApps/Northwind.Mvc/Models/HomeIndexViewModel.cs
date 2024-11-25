efdd8086d0c2103c7d230e6e01e00a3f3ca20d20
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-11-25 11:49:16 -0500

    Added HomeIndexViewModel

---
+++ PracticalApps/Northwind.Mvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,6 @@
+﻿using Northwind.EntityModels; // For Category, product
+
+namespace Northwind.Mvc.Models
+{
+    public record HomeIndexViewModel(int VisitorCount, IList<Category> Categories, IList<Product> Products);
+}
