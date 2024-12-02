43f7a959dc77cfc277cb5ce5ddfe17ee1d0e8fbc
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-12-02 08:49:56 -0500

    Added get single and create endpoints

--- PracticalApps/Northwind.WebApi/Controllers/CustomersController.cs
+++ PracticalApps/Northwind.WebApi/Controllers/CustomersController.cs
@@ -22,7 +22,7 @@ namespace Northwind.WebApi.Controllers
         // Always returns list of customers, may be empty
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Customer>))]
-        public async Task<IEnumerable<Customer>> GetCustomer(string? country)
+        public async Task<IEnumerable<Customer>> GetCustomers(string? country)
         {
             if (string.IsNullOrWhiteSpace(country))
             {
@@ -32,6 +32,45 @@ namespace Northwind.WebApi.Controllers
             {
                 return (await _repo.RetrieveAllAsync())
                     .Where(customer => customer.Country == country);
+            }
+        }
+
+        // GET: api/customers/[id]
+        [HttpGet("{id}", Name = nameof(GetCustomer))] // Named route
+        [ProducesResponseType(200, Type = typeof(Customer))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetCustomer(string id)
+        {
+            Customer? c = await _repo.RetrieveAsync(id);
+            if (c == null)
+            {
+                return NotFound();
+            }
+            return Ok(c);
+        }
+
+        // POST: api/customers
+        // BODY: Customer (JSON, XML)
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Customer))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Create([FromBody] Customer c)
+        {
+            if (c == null)
+            {
+                return BadRequest();
+            }
+            Customer? addedCustomer = await _repo.CreateAsync(c);
+            if (addedCustomer == null)
+            {
+                return BadRequest("Repository failed to create customer.");
+            }
+            else
+            {
+                return CreatedAtRoute(
+                    routeName: nameof(GetCustomer),
+                    routeValues: new { id = addedCustomer.CustomerId.ToLower() },
+                    value: addedCustomer);
             }
         }
     }
